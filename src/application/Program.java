1229a1456700b1cd1e3101f318895d036d5a5def
Paulo Souza <paulohrs30@gmail.com>
2021-05-28 16:00:39 -0300

    uPDATE IMPLEMENTATION

--- src/application/Program.java
+++ src/application/Program.java
@@ -34,11 +34,16 @@ public class Program {
 			System.out.println(obj);
 		}
 		
-		System.out.println("\n=== TEST 3: seller findAll ===");
+		System.out.println("\n=== TEST 3: seller isnert ===");
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted New Id = " + newSeller.getId());
 
+		System.out.println("\n=== TEST 5: seller update ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Wayne");
+		sellerDao.update(seller);
+		System.out.println("Update completed!");
 	}
 
 }
